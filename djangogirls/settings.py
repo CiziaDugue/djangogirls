53281215086f8012a69fb5560549aefd7e69af4f
Cizia Dugué <cizia.dugue@telecom-st-etienne.fr>
2019-03-27 15:00:40 +0100

    db conf

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'djangogirls.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'djangogirls',
+#         'USER': 'djangogirls',
+#         'PASSWORD': 'djangogirls',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangogirls',
-        'USER': 'djangogirls',
+        'USER': 'CiziaDugue',
         'PASSWORD': 'djangogirls',
-        'HOST': 'localhost',
+        'HOST': 'CiziaDugue.mysql.pythonanywhere-services.com',
         'PORT': '5432',
     }
 }
